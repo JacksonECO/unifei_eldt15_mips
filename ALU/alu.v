6a7d81fb434ce930af5524fc0e006c1e127e6cd9
Jackson Galdino <jacksongdas.jackson@gmail.com>
2022-07-09 17:11:53 -0300

    add ALU

---
+++ ALU/alu.v
@@ -0,0 +1,16 @@
+module alu (
+		input 	  [31:0] data1, data2,
+		input 	  [1:0] sel,
+		output reg [31:0] out
+	);
+
+		always @(*) begin
+			case(sel)
+				2'b00: out <= data1 + data2; //Soma
+				2'b01: out <= data1 - data2;	//Subtração
+				2'b10: out <= data1 & data2; //AND
+				2'b11: out <= data1 | data2; //OR
+			endcase			
+		end
+
+endmodule 
