f082225fe88d50562b7ad7ee3ff95b7c1024ca4e
Jackson Galdino <jacksongdas.jackson@gmail.com>
2022-07-09 17:58:19 -0300

    add Register

---
+++ Register/Register.v
@@ -0,0 +1,12 @@
+module Register(
+		input rst, clk,
+		input [31:0] D,
+		output reg  [31:0] Q
+	);
+		
+	always @(posedge clk or posedge rst) begin
+		if (rst) Q <= 32'b0;
+		else     Q <= D;
+	end
+	
+endmodule 
