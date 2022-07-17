1f9dc65397963b2cb3b31e195230765f3b515fa1
Jackson Galdino <jacksongdas.jackson@gmail.com>
2022-07-17 14:41:56 -0300

    Modificando o device do instructionMemory e do PC Melhorei o testbench do instructionMemory

--- InstructionMemory/instructionmemory_TB.v
+++ InstructionMemory/instructionmemory_TB.v
@@ -12,61 +12,12 @@ module instructionmemory_TB();
 		
 	);
 	
-	initial begin
-		Addr=0;
-		#10;
-		Addr=1;
-		#10;
-		Addr=2;
-		#10;
-		Addr=3;
-		#10;
-		Addr=4;
-		#10;
-		Addr=5;
-		#10;
-		Addr=6;
-		#10;
-		Addr=7;
-		#10;
-		Addr=8;
-		#10;
-		Addr=9;
-		#10;
-		Addr=10;
-		#10;
-		Addr=11;
-		#10;
-		Addr=12;
-		#10;
-		Addr=13;
-		#10;
-		Addr=14;
-		#10;
-		Addr=15;
-		#10;
-		Addr=16;
-		#10;
-		Addr=17;
-		#10;
-		Addr=18;
-		#10;
-		Addr=19;
-		#10;
-		Addr=20;
-		#10;
-		Addr=21;
-		#10;
-		Addr=22;
-		#10;
-		Addr=23;
-		#10;
-		Addr=24;
-		#10;
-		Addr=25;
-		
-	end
 	
-	initial #500 $stop;
+	initial begin
+		Addr = 0;
+		#500 $stop;
+	end 
+
+	always #20 Addr = Addr + 1;
 	
 endmodule
